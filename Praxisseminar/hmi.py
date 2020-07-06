8bc73039608c2b2e2be87a142ef3f1c82093d827
Johannes Seitz <johannes.seitz@st.oth-regensburg.de>
2020-07-06 20:03:16 +0200

    Update 06.07. 20:00

--- Praxisseminar/hmi.py
+++ Praxisseminar/hmi.py
@@ -10,7 +10,7 @@ from utils import STATE, PLC1_DATA, PLC1_PROTOCOL, PLC1_ADDR
 
 import get_ip
 
-import sys
+#import sys
 import time
 
 
@@ -55,8 +55,17 @@ class PHMI(HMI):
             print
             eingabe = str(input("Auslesen Status: Taste 1/ Geschwindigkeit einstellen: Taste 2/ Ein-/Ausschalten: Taste 3/ Programm beenden: Taste 99 "))
 
+            # evtl doch auf switch ... case umsteigen - geht nicht so wie ich mir das vorstelle
+
+            # Status abfragen (Ein
             if eingabe == '1':
-                motor = self.get(MOTOR_1)
+                motor = self.receive(MOTOR_1, PLC1_ADDR)
+                print "DEBUG plc1 erhält motor: " + motor
+                Praxisseminar_test_logger.debug('Motor_1: ' + motor)
+
+                # wenn mit eigenem Status-Bereich gearbeitet wird
+                #self.send(MOTOR_1, motor, HMI_ADDR)
+
                 if motor == 1:
                     print 'DEBUG plc1 motor: An'
                 elif motor == 0:
@@ -65,18 +74,48 @@ class PHMI(HMI):
                 Praxisseminar_test_logger.debug('Motor_1: ' + motor)
 
             elif eingabe == '2':
-                motor = self.get(MOTOR_1)
+                motor = self.receive(MOTOR_1, PLC1_ADDR)
+                print "DEBUG plc1 erhält motor: " + motor
+                Praxisseminar_test_logger.debug('Motor_1: ' + motor)
+
+                # siehe Eingabe '1'
+
                 if motor == 1:
-                    sensor = self.get(SENSOR_1)
+                    sensor = float(self.receive(SENSOR_1, PLC1_ADDR))
                     print 'DEBUG plc1 motor: An mit der Geschwindigkeit' + sensor
+                    Praxisseminar_test_logger.debug('Sensor_1: ' + sensor)
+
+                    # Wollen Sie die Geschwindigkeit verändern? Wie hoch soll die Geschwindigkeit sein (Rahmen der Geschwindigkeit anpassen)
+                    change = input("Wollen Sie die Geschwindigkeit veraendern? J/N")
+
+                    if change == "J" or change == "j":
+                        new_vel = float(input("Geben Sie die neue Geschwindigkeit ein: "))
+                        self.set(SENSOR_1, new_vel)
+                        self.send(SENSOR_1, new_vel, PLC1_ADDR)
+                        print 'DEBUG plc1 motor: An mit neuer Geschwindigkeit' + new_vel
+                        Praxisseminar_test_logger.debug('Sensor_1: ' + new_vel)
+
+                    elif change == "N" or change == "n":
+                        break
 
                 elif motor == 0:
                     print 'DEBUG plc1 motor: Aus'
                 print
                 Praxisseminar_test_logger.debug('Motor_1: ' + motor)
 
+            elif eingabe == '3':
+                motor = self.receive(MOTOR_1, PLC1_ADDR)
+                print "DEBUG plc1 erhält motor: " + motor
+                Praxisseminar_test_logger.debug('Motor_1: ' + motor)
 
-            print "TODO HMI main_loop: please override me"
+                if motor == 1:
+                    onoff = input("Wollen Sie den Motor aus oder einschalten? Aus = 0 /Ein = 1")
+
+                    if onoff == 0:
+                        self.send(MOTOR_1, onoff, PLC1_ADDR)
+                    elif onoff == 1:
+                        self.send(MOTOR_1, onoff, PLC1_ADDR)
+
             time.sleep(sleep)
 
             sec += 1
