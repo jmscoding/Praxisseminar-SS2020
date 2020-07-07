4d803cad546a4bc7d572c18b1d19401e512f4e66
Johannes Seitz <johannes.seitz@st.oth-regensburg.de>
2020-07-07 14:49:11 +0200

    Update 07.07. 14:50

--- Praxisseminar/get_ip.py
+++ Praxisseminar/get_ip.py
@@ -5,6 +5,8 @@ from utils import PLC1_ADDR
 
 def get_ip():
     for ifaceName in interfaces():
+
+        # Urspruenglicher Code
         #addresses = [i['addr'] for i in ifaddresses(ifaceName).setdefault(AF_INET, [{'addr':'No IP addr'}] )]
         #print '%s: %s' % (ifaceName, ', '.join(addresses))
         #print ifaceName
@@ -17,15 +19,17 @@ def get_ip():
 
             #print i['addr']
             if (i['addr'] != '127.0.0.1') or (i['addr'] != PLC1_ADDR):
-                return i['addr']
+                return str(i['addr'])
 
     return 0
 
 
 
 
+#Funktionen zum Testen des Codes
+
 #ip = get_ip()
-#ip1 = str(ip[0])
+#ip1 = str(ip)
 
 
 #print ip1
