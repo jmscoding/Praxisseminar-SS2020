e6913eeccec50584fa70024b6564b42fe99e67bb
Johannes Seitz <johannes.seitz@st.oth-regensburg.de>
2020-07-08 15:31:38 +0200

    Update 08.07. 15:30

--- STIX/bundle_creator.py
+++ STIX/bundle_creator.py
@@ -1,32 +1,81 @@
 # erstellt ein Bundle eines Stix Vorfalls
 
-from stix2 import (ThreatActor, Identity, Relationship, Bundle)
+from stix2 import (ThreatActor, Identity, Relationship, Bundle, AttackPattern, ExternalReference)
+
+# automatische Uebernahme der Zeitangabe und wann es modifiziert wurde
+# automatisches Hinzufuegen einer ID
+
+# Fragen
+# 1. muss id nach RFC 4122-complient UUID aufgebaut werden
+#
 
 threat_actor = ThreatActor(
-    id="threat-actor--dfaa8d77-07e2-4e28-b2c8-92e9f7b04428",
-    created="2014-11-19T23:39:03.893Z",
-    modified="2014-11-19T23:39:03.893Z",
-    name="Disco Team Threat Actor Group",
-    description="This organized threat actor group operates to create profit from all types of crime.",
-    threat_actor_types=["crime-syndicate"],
-    aliases=["Equipo del Discoteca"],
+    id="threat-actor--1",
+    created="2020-07-08T23:39:03.893Z",
+    modified="2020-07-08T23:39:03.893Z",
+    name="Innentaeter",
+    description="Ein Innentaeter versucht den aktiven Prozess mitzuhoeren, Informationen zu gewinnen und Daten zu manipulieren.",
+    threat_actor_types=["insider-disgruntled"], #muss noch geaendert werden
+    aliases=["Verraeter"], #muss noch geaendert werden
     roles=["agent"],
-    goals=["Steal Credit Card Information"],
+    goals=["Informationen ueber den Netzwerkverkehr im Unternehmen gewinnen", "Daten manipulieren"],
     sophistication="expert",
     resource_level="organization",
     primary_motivation="personal-gain"
 )
 
-identity = Identity(
-    id="identity--733c5838-34d9-4fbf-949c-62aba761184c",
-    created="2016-08-23T18:05:49.307Z",
-    modified="2016-08-23T18:05:49.307Z",
-    name="Disco Team",
-    description="Disco Team is the name of an organized threat actor crime-syndicate.",
-    identity_class="organization",
-    contact_information="disco-team@stealthemail.com"
+identity1 = Identity( # Angreifer-Firma
+    id="identity--1",
+    created="2020-07-08T18:05:49.307Z",
+    modified="2020-07-08T18:05:49.307Z",
+    name="Firma B",
+    description="Firma B versucht Innentaeter anzuwerben und dadurch der Konkurrenz zu schaden.",
+    identity_class="organization"
+    #contact_information="disco-team@stealthemail.com"
 )
 
-relationship = Relationship(threat_actor, 'attributed-to', identity)
+identity2 = Identity( # Firma die angegriffen wird
+    type="identity",
+    id="identity--2",
+    created="2020-07-08T23:39:03.893Z",
+    modified="2020-07-08T23:39:03.893Z",
+    name="Firma A",
+    description="Firma A ist die angegriffene Firma.",
+    identity_class="organization"
+)
 
-bundle = Bundle(objects=[threat_actor, identity, relationship])
+ref_capec1 = ExternalReference(
+    source_name="capec",
+    url="https://capec.mitre.org/data/definitions/94.html",
+    external_id="CAPEC-94"
+)
+
+ref_capec2 = ExternalReference(
+    source_name="capec",
+    url="https://capec.mitre.org/data/definitions/343.html",
+    external_id="CAPEC-343"
+)
+
+attack_pattern1 = AttackPattern(
+    type="attack-pattern",
+    id="attack-pattern--1",
+    created="2020-07-08T23:39:03.893Z",
+    modified="2020-07-08T23:39:03.893Z",
+    name="Man in the Middle Attack",
+    external_references=[ref_capec1]
+)
+
+attack_pattern3 = AttackPattern(
+    type="attack-pattern",
+    id="attack-pattern--2",
+    created="2020-07-08T23:39:03.893Z",
+    modified="2020-07-08T23:39:03.893Z",
+    name="DOS-Attack",
+    external_references=[ref_capec2]
+)
+
+relationship1 = Relationship(threat_actor, 'attributed-to', identity1)
+relationship2 = Relationship(threat_actor, 'attributed-to', identity2)
+relationship3 = Relationship(threat_actor, 'uses', attack_pattern1)
+
+bundle = Bundle(objects=[attack_pattern1, threat_actor, identity1, identity2, relationship1, relationship2, relationship3])
