a3303aac6e7bdfa2b8401031f342d7c421e10d81
Johannes Seitz <johannes.seitz@st.oth-regensburg.de>
2020-07-09 22:25:36 +0200

    Update 09.07. 22:30

--- Praxisseminar/Packet_Sniffer/general.py
+++ Praxisseminar/Packet_Sniffer/general.py
@@ -12,7 +12,7 @@ def get_mac_addr(mac_raw):
 def format_multi_line(prefix, string, size=80):
     size -= len(prefix)
     if isinstance(string, bytes):
-        string = ''.join(r'\x{:02x}'.format(byte) for byte in string)
+        string = ''.join(r'\x{:02x}'.format(ord(byte)) for byte in string)
         if size % 2:
             size -= 1
     return '\n'.join([prefix + line for line in textwrap.wrap(string, size)])
