d96d610844ca318b2ea3ddacbf30dfe9bb12c0ee
Johannes Seitz <johannes.seitz@st.oth-regensburg.de>
2020-07-14 15:07:32 +0200

    Update Phys. Proz.

--- Praxisseminar/plc1.py
+++ Praxisseminar/plc1.py
@@ -38,32 +38,25 @@ class CbPLC1(PLC):
             Praxisseminar_test_logger.info("Motor erhaelt von PLC1_ADDR: %s" % str(rec_m11))
             self.send(MOTOR, rec_m11, PLC1_ADDR)
             Praxisseminar_test_logger.info("Motor sendet an PLC1_ADDR: %s" % str(rec_m11))
-            print 'DEBUG: Sende %s an PLC1' % str(rec_m11)
+
 
             sensor = self.get(SENSOR)
+            self.send(SENSOR, sensor, PLC1_ADDR)
             Praxisseminar_test_logger.info("Sensor: %s" % str(sensor))
 
             rec_s11 = float(self.receive(SENSOR, PLC1_ADDR))
             # Programmabbruch bei zu hoher Geschwindigkeit
 
             if rec_s11 > MOTOR_VEL['MAX']:
-                print 'DEBUG Praxisseminar plc1 shutdown'
                 Praxisseminar_test_logger.warning('PLC1 shutdown, die Geschwindigkeit hat das Maximum ueberschritten ')
                 self.send(SENSOR, 0.0, PLC1_ADDR)
                 self.set(SENSOR, 0.0)
                 self.send(MOTOR, 0, PLC1_ADDR)
                 self.set(MOTOR, 0.0)
                 break
-
-
-            Praxisseminar_test_logger.info("Motor erhaelt von PLC1_ADDR: %s" % str(rec_m11))
-            Praxisseminar_test_logger.info("Sensor erhaelt von PLC1_ADDR: %s" % str(rec_s11))
-            self.send(SENSOR, rec_s11, PLC1_ADDR)
-            Praxisseminar_test_logger.info("Sensor sendet an PLC1_ADDR: %s" % str(rec_s11))
-            self.set(SENSOR, rec_s11)
-            Praxisseminar_test_logger.info("Sensor setzt : %s" % str(rec_s11))
-
-            print 'DEBUG: Sende %s an PLC1' % str(rec_s11)
+            else:
+                self.set(SENSOR, rec_s11)
+                self.send(SENSOR, rec_s11, PLC1_ADDR)
 
             time.sleep(sleep)
 
